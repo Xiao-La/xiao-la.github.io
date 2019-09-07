172d9e31fb7723602e1bcc1bef5f98433134ddf0
Xiao-La <47178126+Xiao-La@users.noreply.github.com>
2019-09-07 11:08:16 +0800

    updata

--- js/loadblogs.js
+++
@@ -1,6 +0,0 @@
-function getDirectories(path){ 
-    return fs.readdirSync(path).filter(function (file){ 
-    return fs.statSync(path+'/'+file).isDirectory(); 
-    }); 
-} 
-getDirectories("../blogs")
